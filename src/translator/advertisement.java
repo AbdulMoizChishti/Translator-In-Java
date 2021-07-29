f0e0c246a21eccf66944b9de1f53f534b4235a00
AbdulMoizChishti <moaiz599@gmail.com>
2021-07-29 11:37:26 +0500

    Translator In Java

---
+++ src/translator/advertisement.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package translator;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class advertisement {
+    
+    public void modeofAds(){
+        System.out.println("Select any 1 mode \n 1-SMS");
+    }
+    
+}
